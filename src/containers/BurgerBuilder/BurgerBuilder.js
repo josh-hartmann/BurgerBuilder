a90bff0eb3f4f16cb51da6ccfd903050a09c02ea
Josh <hartmann.joshua1998@gmail.com>
2020-04-29 10:58:01 -0400

    Further changes

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,8 @@ import Burger from '../../components/Burger/Burger';
 import Controls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
+import Spinner from '../../components/UI/Spinner/Spinner';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -15,15 +17,17 @@ const INGREDIENT_PRICES = {
 export default class BurgerBuilder extends Component {
 
     state = {
-        ingredients: {
-            salad:0,
-            bacon:0,
-            cheese:0,
-            meat:0,
-        },
+        ingredients: null,
         totalPrice: 4,
         purchaseable: false,
         purchasing: false,
+        loading: false
+    }
+
+    componentDidMount () {
+        axios.get('https://react-my-burger-945ad.firebaseio.com/Ingredients.json').then(response => {
+            this.setState({ingredients: response.data});
+        });
     }
     
     updatePurchaseSate (ingredients) {
@@ -78,34 +82,77 @@ export default class BurgerBuilder extends Component {
         this.setState({purchasing: false})
     }
     purchaseContinuedHandler = () => {
-        alert("You continued");
+        this.setState({loading:true})
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Josh Hartmann',
+        //         address: {
+        //             street: 'Line 2 North',
+        //             country: 'Canada',
+        //             zipCode: 'L0L1X0'
+        //         },
+        //         email: 'josh@test.com'
+        //     },
+        //     deliveryMethod: 'ground'
+        // }
+        // axios.post('/orders.json', order).then(response => {
+        //     this.setState({loading:false, purchasing:false})
+        // }).catch(error => {
+        //     this.setState({loading:false, purchasing: false})
+        // });
+        const queryParams = [];
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
+        }
+        const queryString = queryParams.join('&');
+        this.props.history.push({
+            pathname: 'checkout',
+            search: '?' + queryString
+        });
     }
 
     render() {
         const disabledInfo = {
             ...this.state.ingredients
         };
+        let orderSummary = null;
+        
+
         for (let key in disabledInfo) {
             disabledInfo[key] = disabledInfo[key] <= 0;
         }
+        
+        let burger = <Spinner/>
+
+        if (this.state.ingredients) {
+            burger = <div> <Burger ingredients={this.state.ingredients} />
+            <div>
+                <Controls
+                    ingredientRemoved={this.removeIngredientHandler}
+                    ingredientAdded={this.addIngredientHandler}
+                     disabled={disabledInfo}
+                    price={this.state.totalPrice}
+                    purchaseable={this.state.purchaseable}
+                    order={this.purchaseHandler} />
+            </div>
+        </div>
+        orderSummary = <OrderSummary ingredients = {this.state.ingredients}
+        cancelOrder={this.purchaseCanceledHandler}
+        continueWithOrder = {this.purchaseContinuedHandler}
+        price={this.state.totalPrice}></OrderSummary>;
+        }
+        if (this.state.loading) {
+            orderSummary = <Spinner/>;
+        }
+
         return (
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCanceledHandler}>
-                    <OrderSummary ingredients = {this.state.ingredients}
-                    cancelOrder={this.purchaseCanceledHandler}
-                    continueWithOrder = {this.purchaseContinuedHandler}
-                    price={this.state.totalPrice}></OrderSummary>
+                    {orderSummary}
                 </Modal>
-                <Burger ingredients={this.state.ingredients}/>
-                <div>
-                    <Controls 
-                    ingredientRemoved= {this.removeIngredientHandler} 
-                    ingredientAdded={this.addIngredientHandler}
-                    disabled={disabledInfo}
-                    price={this.state.totalPrice}
-                    purchaseable = {this.state.purchaseable}
-                    order={this.purchaseHandler}/>
-                </div>
+                { burger }
             </Aux>
             
         );
---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Button from '../../../components/UI/Button/Button';
+
+
+class ContactData extends Component {
+    state = {
+        name: '',
+        email: '',
+        address: {
+            street: '',
+            postalCode: '',
+        }
+    }
+    
+    render () {
+        return (
+            <div>
+                <h4>Enter Your Contact Data</h4>
+                <form>
+                    <input type = "text" name="name" placeHolder="Your name"/>
+                    <input type = "email" name="email" placeHolder="Your email"/>
+                    <input type = "text" name="street" placeHolder="Street"/>
+                    <input type = "text" name="postal" placeHolder="Postal Code"/>
+                    <Button btnType="Success">ORDER</Button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default ContactData;
